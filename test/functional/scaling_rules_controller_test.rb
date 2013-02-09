ea4afb3989d5cd1b5394b65535c380da398d5d2c
Dariusz Krol <dkrol@agh.edu.pl>
2013-02-09 08:25:31 +0100

    Scaling rules and cool down period test files

---
+++ test/functional/scaling_rules_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ScalingRulesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/unit/cool_down_period_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CoolDownPeriodTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
